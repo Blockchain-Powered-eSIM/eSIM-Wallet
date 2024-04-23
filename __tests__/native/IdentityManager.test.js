229568e1210a070b19e5e8908a7be0713f604557
ArpitxGit <arpitxdungeon@gmail.com>
2024-04-24 01:06:51 +0530

    working detox setup

--- __tests__/native/IdentityManager.test.js
+++ __tests__/native/IdentityManager.test.js
@@ -1,26 +1,47 @@
-// import {
-//     NativeModules,
-// } from 'react-native';
+import {NativeModules} from 'react-native';
 
-// import { getName } from '../../android/app/src/main/java/com/lpaapp/'
+// import {getName} from '../../android/app/src/main/java/com/lpaapp/IdentityManagerModule.java';
 
-// jest.mock('../../android/app/src/main/java/com/lpaapp/IdentityManager/IdentityManagerModule.java', () => ({
+// jest.mock(
+//   '../../android/app/src/main/java/com/lpaapp/IdentityManager/IdentityManagerModule.java',
+//   () => ({
 //     getName: jest.fn(),
-// }));
+//   }),
+// );
 
 // describe('IdentityManagerNativeModule', () => {
+//   // beforeAll(() => {
+//   //     NativeModules.IdentityManager = {
+//   //         getName: jest.fn(),
+//   //         getDefaultPhoneNumber: jest.fn(),
+//   //     }
+//   // });
 
-//     // beforeAll(() => {
-//     //     NativeModules.IdentityManager = { 
-//     //         getName: jest.fn(),
-//     //         getDefaultPhoneNumber: jest.fn(),
-//     //     } 
-//     // });
-
-//     describe('get name', () => {
-//         it('returns name of the module', async () => {
-//             const moduleName = await NativeModules.IdentityManager.getName();
-//             expect(moduleName).toBe('IdentityManager');
-//         });
+//   describe('get name', () => {
+//     it('returns name of the module', async () => {
+//       const moduleName = await NativeModules.IdentityManager.getName();
+//       expect(moduleName).toBe('IdentityManager');
 //     });
+//   });
 // });
+
+jest.mock('react-native', () => {
+  const RN = jest.requireActual('react-native'); // use original implementation, which comes with mocks out of the box
+
+  // mock modules/components created by assigning to NativeModules
+  RN.NativeModules.ReanimatedModule = {
+    configureProps: jest.fn(),
+    createNode: jest.fn(),
+    connectNodes: jest.fn(),
+    connectNodeToView: jest.fn(),
+  };
+
+  // mock modules created through UIManager
+  RN.UIManager.getViewManagerConfig = name => {
+    if (name === 'SomeNativeModule') {
+      return {someMethod: jest.fn()};
+    }
+    return {};
+  };
+  return RN;
+});
--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,25 +1,23 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
+const baseConfig = require('../base.jest.config.js');
+
+/** @type {import('@jest/types').Config} */
 module.exports = {
+  ...baseConfig,
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/**/*.test.js'],
-  testTimeout: 120000,
+  testTimeout: 180000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
   reporters: ['detox/runners/jest/reporter'],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
-};
-
-const baseConfig = require("../base.jest.config.js");
-
-module.exports = {
-  ...baseConfig,
-  setupFilesAfterEnv: ["./init.ts"],
+  // setupFilesAfterEnv: ['./init.ts'],
   setupFiles: [
-    "<rootDir>/../../../node_modules/react-native/jest/setup.js",
-    "<rootDir>/../../../node_modules/react-native-gesture-handler/jestSetup.js",
-    "<rootDir>/../../../test/setup.ts",
+    '<rootDir>/node_modules/react-native-mmkv-storage/jest/mmkvJestSetup.js',
+  ], // Adjusted path here
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation)/)',
   ],
-  testPathIgnorePatterns: ["/node_modules/"],
 };
